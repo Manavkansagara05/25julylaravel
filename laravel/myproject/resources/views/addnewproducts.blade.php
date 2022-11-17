c8267a5825480468c6e4bd849b9d24163436c717
Manavkansagara05 <93247982+Manavkansagara05@users.noreply.github.com>
2022-11-16 21:05:46 -1200

    data

---
+++ laravel/myproject/resources/views/addnewproducts.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.appaddmin')
+
+@section('content')
+<div class="row">
+
+    <!-- Area Chart -->
+    <div class="col-xl-12 col-lg-12">
+        <div class="card shadow mb-4">
+            <!-- Card Header - Dropdown -->
+            <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                <h6 class="m-0 font-weight-bold text-primary">add new product</h6>
+            </div>
+            <!-- Card Body -->
+            <div class="card-body">
+                <!-- <form action="storeproduct" method="post">
+                    <div class="row mt-3">
+                        <div class="col">
+
+                        </div>
+                    </div>
+                </form> -->
+                {!! Form::open(['url' => 'storeproduct']) !!}
+                <div class="row">
+                    <div class="col-md-8  ">
+
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                {{ Form::label('product title');}}
+                                {{ Form::text("title", "", (['class' => 'form-control',"placeholder"=>"Enter product title"])) }}
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                {{ Form::label('Description');}}
+                                {{ Form::textarea("description", "", (['class' => 'form-control',"placeholder"=>"Enter Description"])) }}
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                {{ Form::label('Price');}}
+                                {{ Form::text("price", "", (['class' => 'form-control',"placeholder"=>"Enter Price"])) }}
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                {{ Form::label('Quantity');}}
+                                {{ Form::text("quantity", "", (['class' => 'form-control',"placeholder"=>"Enter Quantity "])) }}
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                               <input type="submit" class="btn btn-primary" name="btn-save" id="btn-save" value="Add">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
+
+    <!-- Pie Chart -->
+
+</div>
+@endsection
